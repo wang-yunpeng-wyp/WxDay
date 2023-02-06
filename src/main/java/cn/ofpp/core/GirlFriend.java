44e7be5310fbd4ab4357e01aba1fbc87320fb426
wang-yunpeng-wyp <55223352+wang-yunpeng-wyp@users.noreply.github.com>
2023-02-06 14:24:06 +0800

    Create GirlFriend.java

---
+++ src/main/java/cn/ofpp/core/GirlFriend.java
@@ -0,0 +1,20 @@
+package cn.ofpp.core;
+
+/**
+ * 你可以new一个女朋友
+ *
+ * @author DokiYolo
+ * Date 2022-08-22
+ */
+public class GirlFriend extends Friend {
+
+    
+    public GirlFriend(String fullName, String province, String city, String birthday, String loveTime, String userId,String taryIt) {
+        super(fullName, province, city, userId, birthday, loveTime, "女", taryIt);
+    }
+
+    public GirlFriend(String fullName, String province, String city, String birthday, String loveTime, String userId, String templateId,String taryIt) {
+        super(fullName, province, city, userId, birthday, loveTime, "女", templateId, taryIt);
+    }
+
+}
