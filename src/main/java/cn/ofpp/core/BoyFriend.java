9052edba4dcc19ef48103112dd37f226b81150fb
wang-yunpeng-wyp <55223352+wang-yunpeng-wyp@users.noreply.github.com>
2023-09-21 10:43:07 +0800

    Update BoyFriend.java

--- src/main/java/cn/ofpp/core/BoyFriend.java
+++ src/main/java/cn/ofpp/core/BoyFriend.java
@@ -8,13 +8,19 @@ package cn.ofpp.core;
  */
 public class BoyFriend extends Friend {
 
-    public BoyFriend(String fullName, String province, String city, String birthday, String loveTime, String userId,String taryIt) {
+  /*  public BoyFriend(String fullName, String province, String city, String birthday, String loveTime, String userId,String taryIt) {
         super(fullName, province, city, userId, birthday, loveTime, "男",taryIt);
     }
 
     public BoyFriend(String fullName, String province, String city, String birthday, String loveTime, String userId, String templateId,String taryIt) {
         super(fullName, province, city, userId, birthday, loveTime, "男", templateId,taryIt);
+    }*/
+   public BoyFriend(String fullName, String province, String city, String birthday, String loveTime, String userId,String taryIt, String author, String origin, String content) {
+        super(fullName, province, city, userId, birthday, loveTime, "男", taryIt,author,origin,content);
     }
 
+    public BoyFriend(String fullName, String province, String city, String birthday, String loveTime, String userId, String templateId,String taryIt , String author, String origin, String content) {
+        super(fullName, province, city, userId, birthday, loveTime, "男", templateId, taryIt,author,origin,content);
+    }
 
 }
