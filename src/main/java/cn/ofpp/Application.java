fa96a88b10976baab2596ad7f0bb7c8c486537ec
wang-yunpeng-wyp <55223352+wang-yunpeng-wyp@users.noreply.github.com>
2023-12-13 09:32:01 +0800

    Update Application.java
    修复体重过高

--- src/main/java/cn/ofpp/Application.java
+++ src/main/java/cn/ofpp/Application.java
@@ -34,8 +34,8 @@ public class Application {
     public static void main(String[] args) {
 
         String author = "初始体重:134斤";
-        String origin = "今日日体重:136";
-        String content = "今日合同金额：-200元";
+        String origin = "今日日体重:128";
+        String content = "今日合同金额：600元";
         
         Bootstrap.init();
         String nextdata= "2023-12-29";//下次见面时间
